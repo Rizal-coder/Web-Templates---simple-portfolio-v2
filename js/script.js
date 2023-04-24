e79e9ab0dc039b1ce68bb13d55c1123ce868e830
M Rizalu Rohman <rizalhadroh172@gmail.com>
2023-04-24 20:17:38 +0700

    Tambah Efect Parallax To about,jumbotron,portfolio

--- js/script.js
+++ js/script.js
@@ -44,3 +44,39 @@
     });
   });
 })();
+
+// Parallax
+// About
+$(window).on("load", function () {
+  $(".about .card").each(function (i) {
+    setTimeout(function () {
+      $(".about .card").eq(i).addClass("cMuncul");
+    }, 500 * (i + 1));
+  });
+});
+(function () {
+  // Fungsi scroll
+  $(window).scroll(function () {
+    // Jumbotron
+    let wScroll = $(document).scrollTop();
+    $(".jumbotron img").css({
+      transform: `translate(0, ${wScroll / 4}%)`,
+    });
+    $(".jumbotron h1").css({
+      transform: `translate(0, ${wScroll / 2}%)`,
+    });
+    $(".jumbotron p").css({
+      transform: `translate(0, ${wScroll / 1.2}%)`,
+    });
+
+    // Portfolio
+    // console.log(wScroll);
+    if (wScroll > $(".portfolio").offset().top - 180) {
+      $(".portfolio img").each(function (i) {
+        setTimeout(function () {
+          $(".portfolio img").eq(i).addClass("muncul");
+        }, 300 * (i + 1));
+      });
+    }
+  });
+})();
